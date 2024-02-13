459dc8bf94414ea54b0519be46388a27ebaf7ad5
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-13 10:56:04 +0530

    Pattern

---
+++ Patterns/inverted_half_pyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {//loop for rows
+        for (int col = 0; col < n - rows; col++)
+        {//loop for columns
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
