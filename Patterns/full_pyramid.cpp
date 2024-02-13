b147a611bb09bbe7679b8388814d7c4bee47e68b
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-13 13:30:48 +0530

    Pattern

---
+++ Patterns/full_pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int col = 0; col < n - rows - 1; col++)
+        {
+            cout << " ";
+        }
+        for (int col = n - rows - 1; col < n; col++) // for (int col = 0; col < rows+1; col++) can also be used
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
