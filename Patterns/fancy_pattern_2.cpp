e161c7052000f8f78d44073a388e799297a96a4a
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-15 00:10:56 +0530

    Pattern

---
+++ Patterns/fancy_pattern_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int col = 0; col < n - rows + 3; col++)
+        {
+            cout << "*";
+        }
+        for (int col = 0; col < 2 * rows + 1; col++)
+        {
+            if (col % 2 == 0)
+            {
+                cout << rows + 1;
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        for (int col = 0; col < n - rows + 3; col++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
