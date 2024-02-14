18d879c2eb6ff08bd22ff3af1544a3175949a9f0
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-14 18:45:21 +0530

    Pattern

---
+++ Patterns/numeric_hollow_half_pyramid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        if (rows == n - 1)
+        {
+            for (int col = 0; col < rows + 1; col++)
+            {
+                cout << col + 1;
+            }
+        }
+        else
+        {
+            for (int col = 0; col < rows + 1; col++)
+            {
+                if (col == 0 or col == rows)
+                    cout << col + 1;
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
