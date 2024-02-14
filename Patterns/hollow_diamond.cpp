86fdbff7eb5afe95b2020cc78e3104d975c3f98d
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-14 13:35:20 +0530

    Pattern

---
+++ Patterns/hollow_diamond.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int col = 0; col < n - rows - 1; col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < 2 * rows + 1; col++)
+        {
+            if (col == 0 || col == 2 * rows)
+            {
+                cout << "*";
+            }
+
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int col = 0; col < rows; col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < 2 * (n - rows) - 1; col++)
+        {
+            if (col == 0 || col == 2 * (n - rows) - 2)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
