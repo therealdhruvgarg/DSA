f5980fc54fba51f4336c62bc6ca041f338b23437
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-14 23:53:44 +0530

    Pattern

---
+++ Patterns/numeric_palindrome_equilateral_pyramid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        int ans = 1;
+        for (int col = 0; col < n - rows - 1; col++)
+        {
+            cout << "  ";
+        }
+        for (int col = n - rows - 2; col < n + rows - 1; col++)
+        {
+            cout << ans << " ";
+            if (col <( (2 * n - 1) / 2)-1)
+                ans++;
+            else
+                ans--;
+        }
+
+        cout << endl;
+    }
+}
