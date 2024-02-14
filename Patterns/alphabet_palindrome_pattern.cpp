54261cd4d1fa00fdf3dccdc82aadc65bfc34a6d1
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-14 18:12:02 +0530

    Pattern

---
+++ Patterns/alphabet_palindrome_pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for (int rows = 0; rows < n; rows++)
+    {
+        char ch = 'A';
+        for (int col = 0; col < 2 * rows + 1; col++)
+        {
+
+            cout << ch << " ";
+            if (col < (2 * rows + 1) / 2)
+            {
+                ch++;
+            }
+            else
+                ch--;
+        }
+        cout << endl;
+    }
+}
