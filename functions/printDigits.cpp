96836ccad580ace4de1a5100939a7c837961cbe2
Dhruv Garg <dhruv.g4445@gmail.com>
2024-02-27 11:00:44 +0530

    funcitions

---
+++ functions/printDigits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define loop1(i, n) for (int i = 1; i <= n; i++)
+using namespace std;
+void printDigits(int n)
+{
+    int digit;
+    while (n != 0)
+    {
+        digit = n % 10;
+        cout << digit << endl;
+        n /= 10;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    printDigits(n);
+}
